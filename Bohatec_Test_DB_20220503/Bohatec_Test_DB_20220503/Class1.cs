f73b0656286aef88b21abd2b6727535e40da49ed
Daniel Bohatec <91957021+dadasus2@users.noreply.github.com>
2022-05-03 15:05:26 +0200

    Add files via upload

---
+++ Bohatec_Test_DB_20220503/Bohatec_Test_DB_20220503/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Bohatec_Test_DB_20220503
+{
+    internal class Class1
+    {
+
+    }
+}
---
+++ Bohatec_Test_DB_20220503/Bohatec_Test_DB_20220503/dbo.Faktury.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Faktury] (
+    [id]          INT           IDENTITY (1, 1) NOT NULL,
+    [datum]       DATE          NULL DEFAULT 15/4/2004,
+    [cislo]       INT           NULL DEFAULT 4568872,
+    [odberatel]   NVARCHAR (30) NULL DEFAULT 'nevim',
+    [nazev]       NVARCHAR (10) NULL DEFAULT 'nevim',
+    [pocet]       INT           NULL DEFAULT 3,
+    [cenaJednoho] FLOAT (53)    NULL DEFAULT 200,
+    [cenaCelkem]  FLOAT (53)    NULL DEFAULT 600,
+    [DPH]         FLOAT (53)    NULL DEFAULT 15,
+    [cenaDPH]     FLOAT (53)    NULL DEFAULT 510,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
